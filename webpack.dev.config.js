fe49266891c9c029dba1a11afe5d4f1bb616c2e2
wangxb153 <wanxgb153@chinaunicom.cn>
2019-12-09 15:31:20 +0800

    配置按需加载，缓存，开发环境构建，优化缓存等功能

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     // 入口
@@ -9,7 +10,8 @@ module.exports = {
     //输出到dist文件夹，输出文件名字是bundle.js
     output: {
         path: path.join(__dirname,'./dist'),
-        filename: 'bundle.js'
+        filename: '[name].[hash].js',
+        chunkFilename: '[name].[chunkhash].js'
     },
     resolve: {
         alias: {
@@ -55,5 +57,9 @@ module.exports = {
         // proxy代理
         proxy: {}
     },
-    devtool: 'inline-source-map'
+    devtool: 'inline-source-map',
+    plugins: [new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: path.join(__dirname, 'src/index.html')
+    })]
 }
