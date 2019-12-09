069b362b606bf25575dbbb95fc19dd7cb9024a93
wangxb153 <wanxgb153@chinaunicom.cn>
2019-12-09 10:20:11 +0800

    配置信息

--- src/router/router.js
+++ src/router/router.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import Home from 'pages/Home/index';
 import Page1 from 'pages/Page1/index';
 import Counter from 'pages/Counter/Counter';
+import getUserInfo from '../pages/UserInfo/UserInfo';
 
 
 const getRouter = () => (
@@ -14,11 +15,13 @@ const getRouter = () => (
                 <li><Link to="/">首页</Link></li>
                 <li><Link to="/page1">Page1</Link></li>
                 <li><Link to="/counter">Counter</Link></li>
+                <li><Link to="/getUserInfo">getUserInfo</Link></li>
             </ul>
             <Switch>
                 <Route exact path="/" component={Home}/>
                 <Route path="/page1" component={Page1}/>
                 <Route path="/counter" component={Counter}/>
+                <Route path="/getUserInfo" component={getUserInfo}/>
             </Switch>
         </div>
     </Router>
